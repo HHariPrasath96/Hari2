f131992f02bf0a5ac22b4fb8bfb66cde9b836d6c
HHariprasath96 <hariprasathalagesan@gmail.com>
2022-11-06 19:25:28 +0530

    Task1

---
+++ src/org/gittcs/Tcs.java
@@ -0,0 +1,39 @@
+package org.gittcs;
+
+public class Tcs {
+
+	private void method() {
+        System.out.println("java");
+	}
+	
+	
+	private void method2() {
+    System.out.println("Pithon");
+    
+	}
+	
+	
+	private void method3() {
+      System.out.println("C#");
+	}
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
